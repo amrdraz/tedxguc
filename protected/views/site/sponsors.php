4af5af03afd821ffeb70278a27f153f25bafc300
Rana Elgohary <rana_elgohary@msn.com>
2013-03-10 17:16:06 +0200

    partners

--- protected/views/site/sponsors.php
+++ protected/views/site/sponsors.php
@@ -6,14 +6,6 @@ $this->page = 'sponsers';
 <div class="sponsors">
 
 	<div class="sponsorContainer">
-		<h3>Sponsors</h3>
-		
-		<div class="imageContainer">
-		<a target="_blank" href="http://www.facebook.com/AlMaqarr"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/sponsors/yourComfortZone.jpg" /></a>
-		</div>
-	</div>
-	
-	<div class="sponsorContainer">
 		<h3>Media Sponsors</h3>
 		
 		<div class="imageContainer">
@@ -21,8 +13,12 @@ $this->page = 'sponsers';
 		</div>
 		
 		<div class="imageContainer">
+		<a target="_blank" href="http://www.facebook.com/AlMaqarr"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/sponsors/yourComfortZone.jpg" /></a>
+		</div>
+		
+		<div class="imageContainer">
 		<a target="_blank" href="http://www.facebook.com/TeenStuffMagazine"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/mediaSponsors/teenstuff.jpg" /></a>
 		</div>
 	</div>
-
+	
 </div>
