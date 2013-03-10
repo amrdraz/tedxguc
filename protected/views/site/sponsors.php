820ba6217add4320965585a3cd28a63ff6eda887
Rana Elgohary <rana_elgohary@msn.com>
2013-03-10 15:32:23 +0200

    Sponsors

--- protected/views/site/sponsors.php
+++ protected/views/site/sponsors.php
@@ -3,4 +3,26 @@ $this->pageTitle=Yii::app()->name . ' - Sponsors';
 $this->page = 'sponsers';
 ?>
 
-<p>If you are interested in sponsoring our event, please email us at <a class="red" href="mailto:fr@tedxguc.com">fr@tedxguc.com</a></p>
+<div class="sponsors">
+
+	<div class="sponsorContainer">
+		<h3>Sponsors</h3>
+		
+		<div class="imageContainer">
+		<a href="http://www.facebook.com/AlMaqarr"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/sponsors/yourComfortZone.jpg" /></a>
+		</div>
+	</div>
+	
+	<div class="sponsorContainer">
+		<h3>Media Sponsors</h3>
+		
+		<div class="imageContainer">
+		<a href="http://www.facebook.com/kelmetnamagazine"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/mediaSponsors/kelmetna.jpg" /></a>
+		</div>
+		
+		<div class="imageContainer">
+		<a href="http://www.facebook.com/TeenStuffMagazine"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/mediaSponsors/teenstuff.jpg" /></a>
+		</div>
+	</div>
+
+</div>
