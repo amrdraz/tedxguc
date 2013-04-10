9a5ddad93e20911a036487c6f927da4c5bb47c06
Rana Elgohary <rana_elgohary@msn.com>
2013-04-11 00:33:49 +0200

    Media Sponsors

--- protected/views/site/sponsors.php
+++ protected/views/site/sponsors.php
@@ -28,6 +28,10 @@ $this->page = 'sponsers';
 		<a target="_blank" href="http://www.facebook.com/QabilaTv"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/mediaSponsors/qabila.jpg" /></a>
 		</div>
 		
+		<div class="imageContainer">
+		<a target="_blank" href="https://m.facebook.com/pages/Mili-Power-Egypt/172610439551640?id=172610439551640&refsrc=http%3A%2F%2Fwww.facebook.com%2Fpages%2FMili-Power-Egypt%2F172610439551640&_rdr"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/mediaSponsors/mili.jpg" /></a>
+		</div>
+		
 	</div>
 	
 	<p class="centredText">If you are interested in sponsoring our event, please email us at <a href="mailto:fr@tedxguc.com">fr@tedxguc.com</a>.</p>
