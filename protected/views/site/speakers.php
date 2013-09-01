4ca8352168eceba47770b9bbee7ef3f482d57071
Rana Elgohary <rana_elgohary@msn.com>
2013-09-01 11:24:00 +0200

    Speakers Page

--- protected/views/site/speakers.php
+++ protected/views/site/speakers.php
@@ -2,3 +2,56 @@
 $this->pageTitle=Yii::app()->name . ' - Speakers';
 $this->page = 'talks';
 ?>
+
+<section class="main-container speakers">
+
+
+	<a href="#">
+		<article class="speakerColumn">
+	
+			<img class="" src="img/speakers/MetwallyMathana.jpg" />
+		
+			<h3>Metwally Mathana</h3>
+			<br/><br/>
+			<p> In 1998 Mahanna was competing in the world cup in Dubai, on his 1st lift he equalized the world record, on the 2nd Mathana broke the world record. Then broke it again. And then for the 3rd time in one night he broke the ... </p>
+	
+		</article>
+	</a>
+	
+	<a href="#">
+		<article class="speakerColumn">
+	
+			<img class="" src="img/speakers/LoayHamed.jpg" />
+		
+			<h3>Loay Hamed</h3>
+			<br/><br/>
+			<p>Loay graduated from the German University in Cairo where he decided to major in electronics engineering. The 25 year old has always been interested in quantum and modern physics along with unusual …</p>
+	
+		</article>
+	</a>
+
+	<a href="#">
+		<article class="speakerColumn">
+	
+			<img class="" src="img/speakers/MostafaHemdan.jpg" />
+		
+			<h3>Mostafa Hemdan</h3>
+			<br/><br/>
+			<p>A young ambitious entrepreneur who led a team of 20 students to found a company for e-waste "electronic waste" recycling. RecycloBekia started with very low capital in a small garage in Egypt, now RecycloBekia is among ...</p>
+	
+		</article>
+	</a>
+	
+	<a href="#">
+		<article class="speakerColumn last">
+	
+			<img class="" src="img/speakers/MetwallyMathana.jpg" />
+		
+			<h3>Murtadha Al-Tameemi</h3>
+			<br/><br/>
+			<p>Murtadha Al-Tameemi of GEMSI (The Global Entrepreneurship and Maker Space Initiative), is one of the first to introduce Hackerspaces, a brand new open- innovation platform, into the Middle East. Contrary to the …  </p>
+	
+		</article>	
+	</a>
+
+</section>
