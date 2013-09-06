80e23f2a75aacd061d8bf457efa6ccb764233101
Rana Elgohary <rana_elgohary@msn.com>
2013-09-06 09:17:50 +0200

    Talks Page

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -23,9 +23,9 @@
 					array('label'=>'Home', 'url'=>array('/site/index')),
 					array('label'=>'About', 'url'=>array('/site/about')),
 					array('label'=>'Team', 'url'=>array('/site/team')),
+					array('label'=>'Events', 'url'=>array('site/events')),
 					array('label'=>'Speakers', 'url'=>array('site/speakers')),
 					array('label'=>'Talks', 'url'=>array('site/talks')),
-					array('label'=>'Events', 'url'=>array('site/events')),
 					array('label'=>'Sponsors', 'url'=>array('site/sponsors')),
 					array('label'=>'Contact', 'url'=>array('/site/contact')),
 					array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
