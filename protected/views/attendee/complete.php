1355df255418188fdc73cb02c084d113b67edaf6
Amr Draz <amr.m.draz@gmail.com>
2014-05-13 21:07:22 +0200

    modified complete

--- protected/views/attendee/complete.php
+++ protected/views/attendee/complete.php
@@ -2,16 +2,9 @@
 $this->pageTitle=Yii::app()->name . ' - Submission Complete';
 ?>
 
-<section class="application applicationComplete main-container">	
 
-<h1 id="submissionAccepted">Submission Accepted, <?php echo $model->name; ?>!
-<br> Thank you for applying =)
-<br> 
-<?php if($model->gucian) { ?>
-	Once your application is accepted, we will let you know what are the procedures for receiving your invitation.
-<?php } else { ?>
-	Don't forget to pass by at the platform area to ask about your ticket.
-<?php } ?>
-</h1>
+<section class="application main-container">	
+
+<h1>Submission Complete</h1>
 
 </section>
